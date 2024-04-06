6466fbe80d0af098a23836d9214f6f32e0f84fc9
Vander Santos <vanderms.84@gmail.com>
2024-04-05 23:49:00 -0300

    partial: banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+  <picture
+    class="w-full h-[30.125rem] md:h-[33.75rem] lg:h-[45.625rem] absolute top-0 left-0 z-[-1]"
+    aria-hidden="true"
+  >
+    <source
+      media="(min-width: 72rem)"
+      srcset="/images/background-pattern/desktop.svg"
+    />
+    <source
+      media="(min-width: 45rem)"
+      srcset="/images/background-pattern/tablet.svg"
+    />
+    <img
+      class="object-fit object-cover w-full h-full"
+      src="/images/background-pattern/mobile.svg"
+      alt=""
+    />
+  </picture>
+  <nav class="w-layout-md mx-auto mt-8">
+    <ul class="flex items-center justify-between">
+      <li>
+        <a href="/" aria-label="Home">
+          <img src="/images/logo-light.svg" alt="" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="/"
+          class="block font-sans text-base leading-loose border-b-[3px] border-primary-700 text-neutral-25 hover:text-primary-700 font-bold"
+          >Apply for access
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <div class="max-w-[21.4375rem] mx-auto mt-16 flex flex-col items-center gap-10">
+    <h1 class="heading-lg text-neutral-25">
+      Data <strong
+        class="font-semibold underline underline-offset-4 decoration-primary-700 decoration-[.1875rem]"
+        >tailored</strong
+      > to your needs.
+    </h1>
+    <a
+      href="/"
+      class="px-6 py-3 bg-primary-700 text-neutral-800 inline-grid place-items-center leading-loose font-bold"
+      >Learn More</a
+    >
+  </div>
+</header>
