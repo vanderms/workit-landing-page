fdda346390e3064d54bc1d215edfce60d1d2ac26
Vander Santos <vanderms.84@gmail.com>
2024-04-06 18:06:36 -0300

    feat: landing page

--- src/lib/card.svelte
+++ src/lib/card.svelte
@@ -3,15 +3,17 @@
   export let order: number;
 </script>
 
-<article class="flex gap-6 flex-col items-center md:flex-row">
+<article
+  class="flex gap-6 flex-col items-center md:flex-row lg:flex-col lg:gap-[3.5rem]"
+>
   <div
     aria-hidden="true"
-    class="font-serif text-xl w-12 h-12 border rounded-full grid place-items-center font-semibold text-neutral-800 border-neutral-800 grow-0 shrink-0"
+    class="font-serif text-xl w-12 h-12 border rounded-full grid place-items-center font-semibold text-neutral-800 border-neutral-800 grow-0 shrink-0 lg:w-[3.5rem] lg:h-[3.5rem]"
   >
     {order}
   </div>
-  <div class="text-center md:text-left">
+  <div class="text-center md:text-left lg:text-center">
     <h2 class="heading-sm text-neutral-800">{title}</h2>
-    <p class="body-md text-neutral-600 mt-4"><slot /></p>
+    <p class="body-md text-neutral-600 mt-4 lg:text-center lg:mt-7"><slot /></p>
   </div>
 </article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,19 @@
 
 <header class="relative">
   <div
-    class="bg-neutral-800 absolute w-full h-[30.125rem] md:h-[33.75rem] z-[-2] radius-pattern"
+    class="bg-neutral-800 absolute w-full h-[30.125rem] md:h-[33.75rem] z-[-2] radius-pattern lg:h-[45.625rem]"
   ></div>
   <img
     src="/images/bg-pattern-1.svg"
-    class="hidden md:block md:absolute md:top-[4.5rem] md:left-[-14.25rem] z-[-1]"
+    class="hidden md:block md:absolute md:top-[4.5rem] md:left-[-14.25rem] z-[-1] lg:top-[7.25rem] lg:left-[-8.625rem]"
     alt=""
   />
   <img
     src="/images/bg-pattern-2.svg"
-    class="hidden md:block md:absolute md:top-[15.625rem] md:right-[-5.875rem] z-[-1]"
+    class="hidden md:block md:absolute md:top-[15.625rem] md:right-[-5.875rem] z-[-1] lg:top-[19.625rem] lg:right-[-3rem]"
     alt=""
   />
-  <nav class="w-layout-md mx-auto pt-8">
+  <nav class="w-layout-md mx-auto pt-8 lg:pt-[3.5rem]">
     <ul class="flex items-center justify-between">
       <li>
         <a href="/" aria-label="Home">
@@ -34,28 +34,32 @@
   </nav>
   <div class="w-layout-md mx-auto mt-16 flex flex-col items-center">
     <div class="flex flex-col items-center">
-      <h1 class="heading-lg text-neutral-25 text-center md:w-[31.25rem]">
+      <h1
+        class="heading-lg text-neutral-25 text-center md:w-[31.25rem] lg:w-[37.5rem]"
+      >
         Data <strong
           class="font-semibold underline underline-offset-4 decoration-primary-700 decoration-[.1875rem]"
           >tailored</strong
         > to your needs.
       </h1>
 
-      <a href="/" class="mt-10 btn-primary">Learn More</a>
+      <a href="/" class="mt-10 btn-primary lg:mt-[2.3125rem]">Learn More</a>
     </div>
     <img
       src="/images/image-hero.webp"
-      class="w-[20rem] mr-[.1875rem] md:w-[32.125rem] mt-16"
+      class="w-[20rem] self-end mr-[.1875rem] md:w-[32.125rem] mt-16 lg:mt-[3.125rem] lg:w-[47.9375rem] lg:mr-[9.1875rem]"
       alt=""
     />
   </div>
 </header>
 <main>
-  <div class="relative pt-16 pb-[4.5rem]">
+  <div class="relative pt-16 pb-[4.5rem] lg:pt-[5.625rem] lg:pb-[10.6875rem]">
     <div
       class="bg-neutral-100 absolute w-full z-[-3] radius-pattern left-0 top-[-20rem] bottom-0"
     ></div>
-    <ol class="w-layout-md mx-auto grid gap-10">
+    <ol
+      class="w-layout-md mx-auto grid gap-10 lg:grid-cols-3 lg:gap-[1.625rem]"
+    >
       <li>
         <Card title="Actionable insights" order={1}
           >Optimize your products, improve customer satisfaction and stay ahead
@@ -78,42 +82,48 @@
       </li>
     </ol>
   </div>
-  <div class="mt-[6.25rem] w-layout-md md:w-[43rem] mx-auto flex flex-col">
-    <div
-      class="w-[81.924198250728862974%] aspect-square rounded-full bg-primary-700 self-center md:w-[17.5625rem] md:self-start"
-    ></div>
+  <div
+    class="mt-[6.25rem] w-layout-md md:w-[43rem] mx-auto flex flex-col lg:mt-[6.875rem] lg:w-[69.5rem]"
+  >
+    <img
+      src="/images/image-founder.webp"
+      alt=""
+      class="w-[81.924198250728862974%] aspect-square rounded-full self-center md:w-[17.5625rem] md:self-start lg:w-[29.8125rem]"
+    />
     <section
-      class="relative bg-neutral-800 text-neutral-25 p-8 mt-[-3.3125rem] flex flex-col md:w-[32.125rem] md:self-end md:mt-[-8.125rem] md:pt-12 md:pb-[3.5rem] md:px-[3.5rem]"
+      class="relative bg-neutral-800 text-neutral-25 p-8 mt-[-3.3125rem] flex flex-col md:w-[32.125rem] md:self-end md:mt-[-8.125rem] md:pt-12 md:pb-[3.5rem] md:px-[3.5rem] lg:mt-[-19rem] lg:w-[45.625rem] lg:p-16"
     >
       <h2 class="heading-md text-center md:text-left">Be the first to test</h2>
-      <p class="body-md text-center mt-4 md:text-left">
+      <p class="body-md text-center mt-4 md:text-left lg:mt-8">
         Hi, I'm Louis Graham, the founder of the company. Book a demo call with
         me to become a beta tester for our app and kickstart your company. Apply
         for access below and I’ll be in touch to schedule a call.
       </p>
-      <a href="/" class="mt-6 btn-primary self-center md:self-start"
+      <a href="/" class="mt-6 btn-primary self-center md:self-start lg:mt-8"
         >Learn More</a
       >
       <img
         src="/images/bg-pattern-3.svg"
-        class="hidden md:block md:absolute md:bottom-[-6.5rem] md:right-[.75rem]"
+        class="hidden md:block md:absolute md:bottom-[-6.5rem] md:right-[.75rem] lg:bottom-[-3.875rem] lg:right-[4.5rem]"
         alt=""
       />
     </section>
   </div>
 </main>
-<footer class="flex flex-col items-center gap-14 mt-[4.625rem] mb-16 md:mt-20">
+<footer
+  class="flex flex-col items-center gap-14 mt-[4.625rem] mb-16 md:mt-20 lg:mt-[7.625rem] lg:mb-[6.875rem]"
+>
   <a href="/" aria-label="Home" class="w-fit">
     <img src="/images/logo-dark.svg" alt="" />
   </a>
   <ul class="flex items-center gap-7 justify-center">
-    <li>
+    <li class="h-5">
       <a href="/" class="icon-social-media facebook" aria-label="Facebook"></a>
     </li>
-    <li>
+    <li class="h-5">
       <a href="/" class="icon-social-media twitter" aria-label="Twitter"></a>
     </li>
-    <li>
+    <li class="h-5">
       <a href="/" class="icon-social-media instagram" aria-label="Instagram"
       ></a>
     </li>
